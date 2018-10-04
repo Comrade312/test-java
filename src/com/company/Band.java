8611719be938d230a57602e72db79c7b5cb37514
Comrade312 <comrade3152@gmail.com>
2018-10-04 10:57:06 +0300

    123

---
+++ src/com/company/Band.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Band {
+    private String name;
+    ArrayList<Artist> bandPart = new ArrayList<Artist>();
+
+
+}
--- src/com/company/Participant.java
+++ src/com/company/Participant.java
@@ -1,5 +1,10 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Participant extends Person{
+    private Date birthday;
+    private ArrayList<Festival> partFest= new ArrayList<Festival>();
 
 }
